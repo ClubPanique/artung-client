9324c28182dcf6b09da4fd69dee7bf2a0734ff31
Alice <alice.leon10@gmail.com>
2020-02-06 21:21:38 +0100

    Ajout de fontawesome - exemples dans HelloWorld.vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+// Imports fontawesome
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+// Définir ici les icones à utiliser.
+import {faHome, faHeart} from '@fortawesome/free-solid-svg-icons';
+import {faHeart as farHeart} from '@fortawesome/free-regular-svg-icons';
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+// Ajouter les icones à la librairie
+library.add(faHome, faHeart, farHeart);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
