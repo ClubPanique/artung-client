c40ef3807e451a48a55f7d6f937961ac4e62c03b
Steve <sagmana@gmail.com>
2020-02-12 23:11:20 +0100

    construction d'un ensemble label avec typage variable et placeholder

--- src/components/forms/FormGroup.vue
+++ src/components/forms/FormGroup.vue
@@ -1,16 +1,37 @@
 <template>
   <div>
-    
+    <div>
+      <label for="contenu">{{text}}</label>
+    </div>
+    <input id="contenu" type="text" placeholder="Ecrivez ici" />
   </div>
 </template>
+  <!-- Types : text, password, email -->
 
 <script>
 export default {
-  name: 'FormGroup',
+  name: "FormGroup",
+  props: {
+    text: String,
+    typeStatus: String
+  },
+  mounted() {
+    this.typeChange();
+  },
+  methods: {
+    typeChange: function() {
+      if (this.typeStatus == "email") {
+        this.$el.querySelector("button").setAttribute("type", "email");
+      } else if (this.typeStatus == "password") {
+        this.$el.querySelector("button").setAttribute("type", "password");
+      } else {
+        this.$el.querySelector("button").setAttribute("type", "text");
+      }
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
