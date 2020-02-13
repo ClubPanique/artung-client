0c149acd255d9a7dd887a0b6a9a431d287d50069
Steve <sagmana@gmail.com>
2020-02-13 22:48:31 +0100

    base avec affichage icones brands

--- src/components/forms/FormGroupInline.vue
+++ src/components/forms/FormGroupInline.vue
@@ -1,16 +1,44 @@
 <template>
   <div>
-    
+    <font-awesome-icon :icon="['fab', 'facebook']" />
+    <font-awesome-icon :icon="['fab', 'twitter']" />
+    <font-awesome-icon :icon="['fab', 'youtube']" />
+    <font-awesome-icon :icon="['fab', 'wordpress']" />
+    <div>
+      <label for="contenu">{{text}}</label>
+    </div>
+    <input id="contenu" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FormGroupInline',
+  name: "FormGroupInline",
+  props: {
+    text: String,
+    iconStatus: String
+  },
+  mounted() {
+    this.iconChange();
+  },
+  methods: {
+    iconChange: function() {
+      if (this.typeStatus == "facebook") {
+        // icon facebook
+      } else if (this.typeStatus == "twitter") {
+        // icon twitter
+      } else if (this.typeStatus == "youtube") {
+        // icon youtube
+      } else if (this.typeStatus == "wordpress") {
+        // icon wordpress
+      } else {
+        // icon vide ou ?
+      }
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
