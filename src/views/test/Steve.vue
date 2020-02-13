4b77256605a10f440377f2b4b5db656112e89a0d
Steve <sagmana@gmail.com>
2020-02-13 22:49:04 +0100

    tests en tout genre

--- src/views/test/Steve.vue
+++ src/views/test/Steve.vue
@@ -1,22 +1,34 @@
 <template>
-  <div>    
-    <FormGroup></FormGroup>
+  <div>
+    <FormGroupInline></FormGroupInline>
+    <FormGroup typeStatus="password"></FormGroup>
+    <Checkbox></Checkbox>
+    <TextButton secondary form></TextButton>
   </div>
 </template>
 
 <script>
-// import TextButton from '../../components/buttons/TextButton'
-// import Checkbox from '../../components/forms/Checkbox''
+import TextButton from '../../components/buttons/TextButton'
+import Checkbox from '../../components/forms/Checkbox'
 import FormGroup from '../../components/forms/FormGroup'
+import FormGroupInline from '../../components/forms/FormGroupInline'
 
 export default {
   name: 'Steve',
   components: {
-    FormGroup
+    FormGroup,
+    Checkbox,
+    TextButton,
+    FormGroupInline
+  },
+data() {
+    return {
+      typeStatus: "text mais il s'en fout..."
+    };
   }
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 
 </style>
