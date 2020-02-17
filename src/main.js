2cc90e7b29022a606720f2a02a93d78d64a79c0e
Steve <sagmana@gmail.com>
2020-02-17 21:28:54 +0100

    ajout icone check

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 // Définir ici les icones à utiliser.
-import { faHome, faHeart, faMusic } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faHeart, faMusic, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 import { faFacebook, faTwitter, faYoutube, faWordpress } from '@fortawesome/free-brands-svg-icons';
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 // Ajouter les icones à la librairie
-library.add(faHome, faHeart, farHeart, faMusic, faFacebook, faTwitter, faYoutube, faWordpress);
+library.add(faHome, faHeart, farHeart, faMusic, faFacebook, faTwitter, faYoutube, faWordpress, faCheck);
 
 Vue.config.productionTip = false;
 
